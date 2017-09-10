e47431446e9652715c05e99aed2f699cbed28301
ustartest <ustilkoartem@gmail.com>
2017-09-10 13:29:57 +0300

    CAPTCH was changed to IMAGE

--- application/forms/Guestbook.php
+++ application/forms/Guestbook.php
@@ -30,16 +30,31 @@ class Application_Form_Guestbook extends Zend_Form
             'validators' => array('EmailAddress',)
         ));
 
-        // Add a captcha
+//         Add a captcha
+//        $this->addElement('captcha', 'captcha', array(
+//            'label'      => 'Введите 5 цифр, которвые вы видите снизу:',
+//            'required'   => true,
+//            'captcha'    => array(
+//                'captcha' => 'Figlet',
+//                'wordLen' => 5,
+//                'timeout' => 300
+//            )
+//        ));
+
         $this->addElement('captcha', 'captcha', array(
-            'label'      => 'Введите 5 цифр, которвые вы видите снизу:',
+            'label'      => 'Введите 6 символов, которвые вы видите снизу:',
             'required'   => true,
-            'captcha'    => array(
-                'captcha' => 'Figlet',
-                'wordLen' => 5,
-                'timeout' => 300
-            )
+            'captcha' => array(
+                'captcha' => 'Image',
+                'wordLen' => 6,
+                'timeout' => 300,
+                'width' => 300,
+                'height' => 100,
+                'imgUrl' => '/captcha',
+                'imgDir' => APPLICATION_PATH . '/../public/captcha',
+                'font' => APPLICATION_PATH .  '/../public/fonts/LiberationSans-Regular.ttf')
         ));
+
 
         // Add the comment element
         $this->addElement('textarea', 'comment', array(
@@ -59,6 +74,19 @@ class Application_Form_Guestbook extends Zend_Form
                 array('validator' => 'StringLength', 'options' => array(0, 16))
             )
         ));
+
+        $this->addElement('text', 'userbrowser', array(
+            'label'      => 'Ваш Browser:',
+            'required'   => true,
+            'filters'    => array('StringTrim'),
+            'validators' => array(
+                array('validator' => 'StringLength', 'options' => array(0, 20))
+            )
+        ));
+
+        $this->setDefault("userbrowser","Chrome");
+
+        $this->setDefault("userip","localhost");
 
         // Add the comment
         $this->addElement('submit', 'add', array(
